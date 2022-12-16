60cc6d4ba0841dfdef66c0f79ab2047d63bced9e
Ilya Shalyapin <ishalyapin@gmail.com>
2022-12-16 22:28:58 +0500

    gorm sql driver

--- go-gorm/main.go
+++ go-gorm/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"log"
 	"net/http"
@@ -36,10 +37,18 @@ func main() {
 	sqlDB.SetMaxOpenConns(10)
 
 	http.HandleFunc("/", index(db))
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/2", second(sqlDB))
+
+	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic(err)
 	}
+}
+
+func writeError(w http.ResponseWriter, err error) {
+	log.Println(err)
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Write([]byte("Internal Server Error"))
 }
 
 func index(db *gorm.DB) http.HandlerFunc {
@@ -48,17 +57,47 @@ func index(db *gorm.DB) http.HandlerFunc {
 
 		err := db.Limit(100).Find(&words).Error
 		if err != nil {
-			log.Println(err)
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("Internal Server Error"))
+			writeError(w, err)
 			return
 		}
 
 		bytes, err := json.Marshal(words)
 		if err != nil {
-			log.Println(err)
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("Internal Server Error"))
+			writeError(w, err)
+			return
+		}
+
+		w.Write(bytes)
+	})
+}
+
+func second(db *sql.DB) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var words []Word
+
+		rows, err := db.Query("select id, title, content from words limit 100")
+		if err != nil {
+			writeError(w, err)
+			return
+		}
+		defer rows.Close()
+
+		for rows.Next() {
+			var word Word
+			err = rows.Scan(&word.Id, &word.Title, &word.Content)
+			if err != nil {
+				break
+			}
+			words = append(words, word)
+		}
+		if err != nil {
+			writeError(w, err)
+			return
+		}
+
+		bytes, err := json.Marshal(words)
+		if err != nil {
+			writeError(w, err)
 			return
 		}
 
