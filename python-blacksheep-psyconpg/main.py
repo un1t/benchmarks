3a118acded70a84da1663e38110b6c19bdf098ab
Ilya Shalyapin <ishalyapin@gmail.com>
2022-12-17 11:57:53 +0500

    python blacksheep psyconpg

---
+++ python-blacksheep-psyconpg/main.py
@@ -0,0 +1,37 @@
+import os
+from dataclasses import dataclass
+from dotenv import load_dotenv
+from blacksheep import Application
+from psycopg_pool import AsyncConnectionPool
+
+app = Application()
+load_dotenv()
+db_pool = AsyncConnectionPool(os.getenv("DATABASE_URL"), min_size=0, max_size=3)
+
+
+@dataclass
+class Word:
+    id: int
+    title: str
+    content: str
+
+
+@app.route("/")
+async def index():
+    words = []
+
+    async with db_pool.connection() as conn:
+        async with conn.cursor() as cur:
+            await cur.execute("SELECT id, title, content from words limit 100")
+            rows = await cur.fetchall()
+
+    for row in rows:
+        word = Word(id=row[0], title=row[1], content=row[2])
+        words.append(word)
+
+    return words
+
+
+@app.route("/ping")
+def ping():
+    return "OK"
