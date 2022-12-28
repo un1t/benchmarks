958e9ff2d4b298b091bd960e0f6131d317bcdb6b
Ilya Shalyapin <ishalyapin@gmail.com>
2022-12-28 17:27:06 +0500

    max connections

--- rust-actix-tokio-postgres/src/main.rs
+++ rust-actix-tokio-postgres/src/main.rs
@@ -1,7 +1,7 @@
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use serde::Serialize;
-use deadpool_postgres::{Config, ManagerConfig, Pool, RecyclingMethod, Runtime};
-use tokio_postgres::{NoTls};
+use deadpool_postgres::{ManagerConfig, Manager, Pool, RecyclingMethod, Runtime};
+use tokio_postgres::{NoTls, Config};
 use url::{Url};
 
 
@@ -20,15 +20,17 @@ async fn index(pool: web::Data<Pool>) -> impl Responder {
         "SELECT id, title, content FROM words LIMIT 100", &[]
     ).await.unwrap();
 
-    let mut words: Vec<Word> = Vec::new();
-    for row in rows {
-        let word = Word{
-            id:row.get(0),
-            title:row.get(1),
-            content:row.get(2),
-        };
-        words.push(word);
-    }
+    let words: Vec<Word> = rows
+        .iter()
+        .map(
+            |row|
+            Word{
+                id:row.get(0),
+                title:row.get(1),
+                content:row.get(2),
+            }
+        )
+        .collect();
 
     HttpResponse::Ok().json(words)
 }
@@ -47,12 +49,15 @@ async fn main() -> std::io::Result<()> {
     let parsed_url = Url::parse(&database_url).unwrap();
 
     let mut pg_config = Config::new();
-    pg_config.user = Some(parsed_url.username().to_string());
-    pg_config.host = Some(parsed_url.host().unwrap().to_string());
-    pg_config.dbname = Some(parsed_url.path()[1..].to_string());
-    pg_config.manager = Some(ManagerConfig { recycling_method: RecyclingMethod::Fast });
+    pg_config.user(parsed_url.username());
+    pg_config.host(&parsed_url.host().unwrap().to_string());
+    pg_config.dbname(&parsed_url.path()[1..]);
 
-    let pool = pg_config.create_pool(Some(Runtime::Tokio1), NoTls).unwrap();
+    let mgr_config = ManagerConfig {
+        recycling_method: RecyclingMethod::Fast
+    };
+    let mgr = Manager::from_config(pg_config, NoTls, mgr_config);
+    let pool = Pool::builder(mgr).max_size(10).build().unwrap();
 
     println!("Starting server at 8080");
 
