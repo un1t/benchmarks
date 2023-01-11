bed35932d96a4378032b523c268c62cf4a6e7bbf
Ilya Shalyapin <ishalyapin@gmail.com>
2023-01-12 01:03:24 +0500

    asyncpg

--- python-blacksheep-psyconpg/main.py
+++ python-blacksheep-psyconpg/main.py
@@ -1,22 +1,26 @@
 import os
+import asyncpg
 from dataclasses import dataclass
 from dotenv import load_dotenv
 from blacksheep import Application
-from psycopg_pool import AsyncConnectionPool
+
 
 app = Application()
 load_dotenv()
-db_pool = AsyncConnectionPool(os.getenv("DATABASE_URL"), min_size=0, max_size=3)
+
+
+@app.on_start
+async def before_start(application: Application) -> None:
+    app.pool = await asyncpg.create_pool(os.getenv("DATABASE_URL"), min_size=0, max_size=5)
 
 
 @app.route("/")
 async def index():
     words = []
 
-    async with db_pool.connection() as conn:
-        async with conn.cursor() as cur:
-            await cur.execute("SELECT id, title, content from words limit 100")
-            rows = await cur.fetchall()
+    async with app.pool.acquire() as conn:
+        async with conn.transaction():
+            rows = await conn.fetch("SELECT id, title, content from words limit 100")
 
     for row in rows:
         word = {"id": row[0], "title": row[1], "content": row[2]}
